6d03ce5f8b4b3e98cbf0ea839fce35638669a64e
Sebastian Urbanski <sebger82@gmail.com>
2017-04-06 23:50:16 +0200

    szukanie najwiekszej wartosci w tablicach

--- js/app.js
+++ js/app.js
@@ -65,3 +65,22 @@ function titleCase(str) {
 }
 
 titleCase("I'm a little tea pot");
+
+//Return an array consisting of the largest number from each provided sub-array. 
+
+function largestOfFour(arr) {
+  var largestNumber = 0;
+  
+  for (var i=0; i < arr.length; i++) {
+    
+    for (var z = 0; z < arr[i].length; z++){
+      if (arr[i][z] > largestNumber) {
+        largestNumber = arr[i][z];
+      }
+    }
+  }
+
+  return arr.indexOf(largestNumber);
+}
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
