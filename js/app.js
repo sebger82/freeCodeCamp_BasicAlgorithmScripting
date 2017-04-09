3cfd4499e70f6076e222e15af2ea82e624ce3caa
Sebastian Urbanski <sebger82@gmail.com>
2017-04-09 10:12:22 +0200

    sprawdzanie czy elementy tablicy zawieraja sie w innej tablicy

--- js/app.js
+++ js/app.js
@@ -160,3 +160,20 @@ function slasher(arr, howMany) {
 }
 
 slasher([1, 2, 3], 2);
+
+// Return true if the string in the first element of the array contains all of the letters of the string in the second element of the array.
+
+function mutation(arr) {
+  var lookingString = arr[1].toLowerCase().split('');
+  for (var i = 0; i < lookingString.length; i++) {
+    if ( arr[0].toLowerCase().indexOf(lookingString[i]) < 0) {
+      return false;
+    }
+  }
+  return true;
+  
+}
+
+mutation(["hello", "hey"]);
+mutation(["zyxwvutsrqponmlkjihgfedcba", "qrstu"]);
+mutation(["Mary", "Aarmy"]);
